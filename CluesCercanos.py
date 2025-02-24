71871ca42e228af72c392affb7b1404d48317a72
JuanMa <juan.martinez@people-media.com.mx>
2025-02-24 16:59:22 -0600

    first commit

---
+++ CluesCercanos.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+from utils.db import Acceso
+from flask import Blueprint
+from flask_jwt_extended import jwt_required
+from decouple import config
+from utils.CallOpenAI import OpenAIConector
+
+
+Clues_bp = Blueprint('Clues', __name__)
+
+@Clues_bp.route('Cerca/<latitud>/<longuitud>/<distancia>',methods=['GET'])
+def CluesCercanos(latitud,longuitud,distancia):
+    CluesCerca = Acceso().EjecutaStoredProcedure("sp_ObtenerUnidadesMedicasCercanas", [float(latitud),float(longuitud),int(distancia)])
+    return jsonify(CluesCerca)
+    
+    
+
---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_jwt_extended import (
+    JWTManager, create_access_token, jwt_required, get_jwt_identity
+)
+
+
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)  # Habilita CORS para todas las rutas
+    
+    app.config["JWT_SECRET_KEY"] = "PMS2024_PEOPL3M3D14"
+    jwt = JWTManager(app)
+    # Importar y registrar Blueprints
+    from .auth import auth_bp
+    from CluesCercanos import Clues_bp
+ 
+    
+     
+    
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+    app.register_blueprint(Clues_bp, url_prefix="/Clues")
+    
+ 
+    
+    
+    return app
