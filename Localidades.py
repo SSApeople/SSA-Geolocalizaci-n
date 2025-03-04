c401a7be8ef786695fffcae352e2470f0daa3e09
JuanMa <juan.martinez@people-media.com.mx>
2025-03-04 12:21:35 -0600

    se agrega controlador de Localidad

--- Localidades.py
+++ Localidades.py
@@ -5,22 +5,25 @@ from flask_jwt_extended import jwt_required
 from decouple import config
 from utils.CallOpenAI import OpenAIConector
 
-
 Localidades_bp = Blueprint('Ub', __name__)
 
 @Localidades_bp.route('Estados',methods=['GET'])
 def GetEstados():
-    Estados = Acceso().EjecutaStoredProcedure("SP_ObtenerUbicaciones", ["ENTIDAD",None,None])
+    Estados = Acceso().EjecutaStoredProcedure("SP_ObtenerUbicaciones", ["ENTIDAD",None,None,None])
     return jsonify(Estados)
-    
     
 @Localidades_bp.route('Estados/Municipios/<Mun>',methods=['GET'])
 def GetMunicipios(Mun):
-    Estados = Acceso().EjecutaStoredProcedure("SP_ObtenerUbicaciones", ["MUNICIPIO",int(Mun),None])
+    Estados = Acceso().EjecutaStoredProcedure("SP_ObtenerUbicaciones", ["MUNICIPIO",int(Mun),None,None])
     return jsonify(Estados)
-
 
 @Localidades_bp.route('Estados/Municipios/<Mun>/Localidad/<Local>',methods=['GET'])
 def GetLocalidades(Mun,Local):
-    Estados = Acceso().EjecutaStoredProcedure("SP_ObtenerUbicaciones", ["LOCALIDAD",int(Mun),int(Local)])
+    Estados = Acceso().EjecutaStoredProcedure("SP_ObtenerUbicaciones", ["LOCALIDAD",int(Mun),int(Local),None])
+    return jsonify(Estados)
+
+
+@Localidades_bp.route('Localidad/<Localidad>',methods=['GET'])
+def GetLocalidadesEstMun(Localidad):
+    Estados = Acceso().EjecutaStoredProcedure("SP_ObtenerUbicaciones", ["LOC_MUN_ESTA",None, None, Localidad])
     return jsonify(Estados)
